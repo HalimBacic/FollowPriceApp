45aea001a7cdf473e8dfd0fc2f4accd066cc76ce
Halim <33162351+HalimBacic@users.noreply.github.com>
2026-01-12 22:36:10 +0100

    Sort component

---
+++ src/components/sortcomponent/SortComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const SortComponent = ({ options = [], onChange, defaultSort }) => {
+  const [field, setField] = useState(defaultSort?.field ?? "");
+  const [direction, setDirection] = useState(defaultSort?.direction ?? "ASC");
+
+  useEffect(() => {
+    if (!field) {
+      onChange(null);
+    } else {
+      onChange({ field, direction });
+    }
+  }, [field, direction]);
+
+  return (    <div>
+      <select value={field} onChange={e => setField(e.target.value)}>
+        <option value="">Sort by</option>
+        {options.map(opt => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+
+      <button onClick={() =>
+        setDirection(d => (d === "ASC" ? "DESC" : "ASC"))
+      }>
+        {direction}
+      </button>
+    </div>)
+};
+
+export default SortComponent;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,10 +4,12 @@ import service from "../../service/ProductService";
 import Productcard from "../../components/productcard/Productcard";
 import "./home.css";
 import PaginationComponent from "../../components/pagination/PaginationComponent";
+import SortComponent from "../../components/sortcomponent/SortComponent";
 
 const Home = () => {
   const [data, setdata] = useState([]);
   const [page, setPage] = useState(1);
+  const [sort, setSort] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,10 +25,25 @@ const Home = () => {
     setdata(prices);
   }
 
+  function handleSort(sort) {
+    console.log("Sorting by:", sort);
+    setSort(sort);
+  }
+
   return (
     <div>
       <Navigation />
-      <PaginationComponent page={page} totalPages={10} onPageChange={changePage} />
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around", alignItems: "center", margin: "10px" }}>
+        <PaginationComponent
+          page={page}
+          totalPages={10}
+          onPageChange={changePage}
+        />
+        <SortComponent
+          onChange={handleSort}
+          options={[{ label: "Price", value: "price" }]}
+        ></SortComponent>
+      </div>
       <div className="products-grid">
         {data.map((item) => (
           <Productcard productdata={item} />
